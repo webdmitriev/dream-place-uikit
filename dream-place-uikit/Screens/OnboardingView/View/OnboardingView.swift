20cf6e03cd274eab9b8fcbe7c074bba714dc9f32
webdmitriev <webdmitriev@gmail.com>
2025-08-27 10:28:09 +0300

    Send start project

---
+++ dream-place-uikit/Screens/OnboardingView/View/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  dream-place-uikit
+//
+//  Created by Олег Дмитриев on 27.08.2025.
+//
+
+import UIKit
+
+class OnboardingView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+
+
+}
+
